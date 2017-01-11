a78c864f4b034ba36a69602973af5513b6bf1960
parkchohee <coolcho22@naver.com>
2017-01-11 22:48:59 +0900

    setting Gun Rotation

--- DirectXProject/Dx3D/cPlayer.cpp
+++ DirectXProject/Dx3D/cPlayer.cpp
@@ -18,13 +18,17 @@ cPlayer::~cPlayer()
 void cPlayer::Setup()
 {
 	m_pShotGun = new cSkinnedMesh("Gun/", "ShotGun.X");
-	D3DXMATRIXA16 matS, matR, matT, matSRT;
+	D3DXMATRIXA16 matS, matRX, matRY, matT, matSRT;
 	D3DXMatrixScaling(&matS, 0.03f, 0.03f, 0.03f);
-	D3DXMatrixRotationZ(&matR, -D3DX_PI);
-	D3DXMatrixTranslation(&matT, 1, -1, 0);
-	matSRT = matS*matR*matT;
-	D3DXMatrixRotationX(&matR, -D3DX_PI / 2);
-	matSRT = matSRT * matR;
+	D3DXMatrixRotationX(&matRX, D3DX_PI / 2);
+	D3DXMatrixRotationY(&matRY, D3DX_PI);
+	D3DXMatrixTranslation(&matT, 1, 0, 0);
+	//matSRT = matS*matR*matT;
+	//D3DXMatrixRotationX(&matR, -D3DX_PI / 2);
+	//matSRT = matSRT * matR;
+
+	matSRT = matS *matRY * matRX* matT;
+	//matSRT = matS * matT;
 	m_pShotGun->SetSRT(matSRT);
 }
 
